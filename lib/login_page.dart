974c50437e8115041f402bb40738f0ba94934252
Stani0812 <taninat0812@gmail.com>
2023-07-02 15:37:22 +0900

    commit 0702

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -47,6 +47,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Row(
           children: [
@@ -132,7 +133,7 @@ class _LoginPageState extends State<LoginPage> {
                 child: Text("登録ページへ"),
               ),
               const SizedBox(height: 1),
-              //Text(infoText)
+              Text(infoText)
             ],
           ),
         ),
