bc73cd6032511b44576c4eb296b3b6ebc71602e1
sowallows10 <soichirou0630@gmail.com>
2023-07-02 14:44:55 +0900

    completed selectpage

--- lib/select_page.dart
+++ lib/select_page.dart
@@ -16,8 +16,14 @@ class _SelectPage extends State<SelectPage> {
             padding: EdgeInsets.all(32),
             child: Column(
               children: <Widget>[
-                const SizedBox(height: 8),
+                const SizedBox(height: 50),
+                const Text("あなたは...？"),
+                const SizedBox(height: 50),
                 ElevatedButton(
+                  style: ElevatedButton.styleFrom( 
+                  primary:Colors.red,
+                  onPrimary:Colors.white,
+                  minimumSize: Size(130, 60)),
                   onPressed: () async {
                     await Navigator.of(context)
                         .pushNamed("/register", arguments: "受験生");
@@ -26,6 +32,11 @@ class _SelectPage extends State<SelectPage> {
                 ),
                 const SizedBox(height: 32),
                 ElevatedButton(
+                  style: ElevatedButton.styleFrom( 
+                  primary:Colors.orange,
+                  onPrimary:Colors.white,
+                  minimumSize: Size(130, 60)
+                  ),
                   onPressed: () async {
                     await Navigator.of(context)
                         .pushNamed("/register", arguments: "在校生");
@@ -34,6 +45,10 @@ class _SelectPage extends State<SelectPage> {
                 ),
                 const SizedBox(height: 32),
                 ElevatedButton(
+                  style: ElevatedButton.styleFrom( 
+                  onPrimary:Colors.white,
+                  minimumSize: Size(100, 60)
+                  ),
                   onPressed: () async {
                     await Navigator.of(context)
                         .pushNamed("/register", arguments: "ベネッセ関係者");
